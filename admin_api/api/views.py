1c685f28b5cb77ac15af65654d26d1c435eba03a
Freek-H <83078671+Freek-H@users.noreply.github.com>
2025-05-01 00:07:03 +0200

    working version with subprocess for windows

--- admin_api/api/views.py
+++ admin_api/api/views.py
@@ -3,12 +3,15 @@ Contains the views for the Admin API.
 """
 
 import os
+import platform
+import subprocess
 
 from flask import Blueprint
 
-from constants import LOG_FOLDER
+from constants import LOG_FOLDER, MAIN_FOLDER
 
 api = Blueprint("simple_page", __name__, template_folder="templates")
+os_name = platform.system()
 
 
 @api.route("/git-pull")
@@ -17,5 +20,17 @@ def git_pull():
     Perform a git pull and log the result with an isoformate datetime.
     """
     log_file_path = LOG_FOLDER / "gitpull.log"
-    os.system(f'echo -e $"$(date -Iseconds)\n$(git pull)" >> {log_file_path}')
+
+    if os_name == "Windows":
+        subprocess.call(
+            [
+                "C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe",
+                f'(Get-Date -Format s) + "`n" + (git pull) | Add-Content "{log_file_path.relative_to(MAIN_FOLDER)}"',
+            ]
+        )
+    elif os_name == "Linux":
+        os.system(f'echo -e $"$(date -Iseconds)\n$(git pull)" >> {log_file_path}')
+    else:
+        raise OSError("Unexpected Operating System")
+
     return open(log_file_path, encoding="utf-8").read()
