b3074c0a42ee8a6bd94f82230805feeb329ac52b
Freek-H <83078671+Freek-H@users.noreply.github.com>
2025-05-01 01:22:34 +0200

    first version of dashboard

--- constants.py
+++ constants.py
@@ -3,14 +3,18 @@ Functions as a central location to load constants from such as paths to director
  values that are globally configured, and secrets loaded from environment.
 """
 
+import os
 import platform
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 MAIN_FOLDER = Path(__file__).parent
+load_dotenv(MAIN_FOLDER / ".env")
 
 os_name = platform.system()
 if os_name == "Windows":
-    DATA_DIR = MAIN_FOLDER / "data"
+    DATA_DIR = MAIN_FOLDER / "streams"
 elif os_name == "Linux":
     DATA_DIR = Path("~/scripts/prod/logs").expanduser()
 else:
@@ -18,5 +22,14 @@ else:
 
 DATA_DIR.mkdir(exist_ok=True, parents=True)
 
+APP_DATA_DIR = MAIN_FOLDER / "data"
+APP_DATA_DIR.mkdir(exist_ok=True, parents=True)
+IMAGES_FOLDER = APP_DATA_DIR / "images"
+IMAGES_FOLDER.mkdir(exist_ok=True, parents=True)
+
 LOG_FOLDER = MAIN_FOLDER / "logs"
 LOG_FOLDER.mkdir(exist_ok=True, parents=True)
+
+TEMPERATURE_API_ADDRESS = os.getenv(
+    "TEMPERATURE_API_ADDRESS", default="http://127.0.0.1:5001/api"
+)
