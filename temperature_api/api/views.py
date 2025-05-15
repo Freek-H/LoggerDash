c9aa55a2bb6cf22166cac81f7d7e0cf5ee612824
Freek-H <83078671+Freek-H@users.noreply.github.com>
2025-05-15 21:54:56 +0200

    converted streams to get and post

--- temperature_api/api/views.py
+++ temperature_api/api/views.py
@@ -3,7 +3,7 @@
 import csv
 from datetime import datetime
 
-from flask import Blueprint, Response, abort, jsonify
+from flask import Blueprint, Response, abort, jsonify, request
 
 from constants import DATA_DIR
 
@@ -19,8 +19,8 @@ def get_available_streams():
 
 def get_data_from_stream(stream, start_datetime, end_datetime):
     """
-    Returns a dictionary of lists containing all the present data for the requested stream between
-     the start and end datetimes, inclusive.
+    Returns a dictionary of lists containing all the present raw data for the requested stream
+     between the start and end datetimes, inclusive.
     """
     file_paths = []
     for file_path in (DATA_DIR / stream).glob("*.csv"):
@@ -52,32 +52,52 @@ def get_data_from_stream(stream, start_datetime, end_datetime):
     return data
 
 
-@api.route("/streams")
+@api.route("/streams", methods=["GET", "POST"])
 def available_streams():
     """
-    Returns a list of all the streams from which data can be requested.
-    """
-    return jsonify(get_available_streams())
+    GET: Returns a list of all the streams from which data can be requested.
+    POST: Returns all data for the selected stream between the start and end datetimes (inclusive)
+     with the specified granularity.
 
+    Expected application/json:
+    {
+        "stream": string,
+        // Mandatory, allowed values can be requested with GET method
+        "datetimeStart": datetime,
+        // Optional, default "1900-01-01T00:00:00", has to be in isoformat
+        "datetimeEnd": datetime,
+        // Optional, default "2999-01-01T00:00:00", has to be in isoformat
+    }
+    """
+    if request.method == "GET":
+        return jsonify(get_available_streams())
 
-@api.route("/stream/<stream>/<start_datetime>/<end_datetime>")
-def data_for_stream(stream, start_datetime, end_datetime):
-    """
-    Returns all data for the requested stream between the start and end datetimes, inclusive.
-    """
+    data = request.get_json()
+    stream = data.get("stream")
+    if stream is None:
+        return abort(Response("Missing key: stream", 400))
     if stream not in get_available_streams():
         return abort(Response("Invalid stream", 400))
+
+    start_datetime = data.get("datetimeStart", "1900-01-01T00:00:00")
     try:
         start_datetime = datetime.fromisoformat(start_datetime)
     except ValueError:
         return abort(
-            Response("Invalid start datetime, please use isoformat date or datetime.")
+            Response(
+                "Invalid start datetime, please use isoformat date or datetime.", 400
+            )
         )
+
+    end_datetime = data.get("datetimeEnd", "2999-01-01T00:00:00")
     try:
         end_datetime = datetime.fromisoformat(end_datetime)
     except ValueError:
         return abort(
-            Response("Invalid end datetime, please use isoformat date or datetime.")
+            Response(
+                "Invalid end datetime, please use isoformat date or datetime.", 400
+            )
         )
 
+    print(data)
     return jsonify(get_data_from_stream(stream, start_datetime, end_datetime))
