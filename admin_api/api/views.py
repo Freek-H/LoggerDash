0e864fc548022297a2b3a6352e2bef2dd03ea535
Freek-H <83078671+Freek-H@users.noreply.github.com>
2025-05-25 23:40:27 +0200

    git path bugfix

--- admin_api/api/views.py
+++ admin_api/api/views.py
@@ -25,18 +25,13 @@ def git_pull():
         subprocess.call(
             [
                 "C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe",
-                f"cd {MAIN_FOLDER.absolute()}",
-            ]
-        )
-        subprocess.call(
-            [
-                "C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe",
-                f'(Get-Date -Format s) + "`n" + (git pull) | Add-Content "{log_file_path.relative_to(MAIN_FOLDER)}"',
+                f'(Get-Date -Format s) + "`n" + (git -C "{MAIN_FOLDER.absolute()}" pull) | Add-Content "{log_file_path.relative_to(MAIN_FOLDER)}"',
             ]
         )
     elif os_name == "Linux":
-        os.system(f"cd {MAIN_FOLDER.absolute()}")
-        os.system(f'echo "$(date -Iseconds)\n$(git pull)" >> {log_file_path}')
+        os.system(
+            f'echo "$(date -Iseconds)\n$(git -C {repr(str(MAIN_FOLDER.absolute()))} pull)" >> {repr(str(log_file_path))}'
+        )
     else:
         raise OSError("Unexpected Operating System")
 
